ff7cc4a05ab202f7a776f7910cc1c02f1a06f4be
Jose Germán Martínez <jgermanmm@gmail.com>
2023-07-16 23:48:34 -0400

    ready challenge

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer')
+
 require('dotenv').config()
 
+const upload = multer({ dest: 'uploads/' })
 var app = express();
 
 app.use(cors());
@@ -11,7 +14,9 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+});
 
 
 const port = process.env.PORT || 3000;
